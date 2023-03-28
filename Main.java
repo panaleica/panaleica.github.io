8a7e705f88070aa17de3c74b6bfaac6a7e17cc19
zxyue <zxyue@outlook.com>
2023-03-29 01:15:41 +0800

    update

--- Main.java
+++ Main.java
@@ -2,29 +2,62 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Objects;
 
 public class Main {
     final static String URL = "https://panaleica.github.io/";
+    final static File ROOT = new File("music");
+    final static String FILENAME = "music.json";
 
     public static void main(String[] args) throws IOException {
-        File file = new File("music");
-        File[] files = file.listFiles();
-        String str = "";
-        for (int i = 0; i < files.length; i++) {
-            String name = files[i].getName();
-            str += String.format("""
-                    {
-                        "name": "%s",
-                        "url": "%s",
-                        "image": "%s"
-                    },
-                    """, name.substring(0, name.lastIndexOf('.')), URL + "music/" + name, URL + "cover/sunyanzi13.jpg");
+        StringBuilder musicList = getMusicList(ROOT);
+        FileWriter fileWriter = new FileWriter(FILENAME);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        printWriter.print('[');
+        printWriter.print(musicList.substring(0, musicList.length() - 2));
+        printWriter.print(']');
+        printWriter.flush();
+    }
+
+    static StringBuilder getMusicList(File file) {
+        StringBuilder str = new StringBuilder();
+        if (file.isDirectory() && Objects.requireNonNull(file.list()).length > 0) {
+            File[] files = file.listFiles();
+            assert files != null;
+            if (files[0].isFile()) {
+                return printList(file);
+            } else {
+                for (File file1 : files) {
+                    str.append(getMusicList(file1));
+                }
+            }
         }
-        FileWriter fw = new FileWriter("list.json");
-        PrintWriter pw = new PrintWriter(fw);
-        pw.print('[');
-        pw.print(str.substring(0, str.length() - 2));
-        pw.print(']');
-        pw.flush();
+        if (file.isDirectory() && Objects.requireNonNull(file.list()).length == 0) {
+            return str;
+        }
+        return str;
+    }
+
+    static StringBuilder printList(File file) {
+        StringBuilder str = new StringBuilder();
+        File[] files = file.listFiles();
+        String coverName = "";
+        assert files != null;
+        for (File file1 : files) {
+            if (file1.getName().endsWith(".jpg") || file1.getName().endsWith(".png")) {
+                coverName = file1.getName();
+            }
+        }
+        for (File file1 : files)
+            if (!file1.getName().endsWith(".jpg") && !file1.getName().endsWith(".png"))
+                str.append(String.format("""
+                        {
+                            "name": "%s",
+                            "url": "%s",
+                            "image": "%s"
+                        },
+                        """, file1.getName().substring(0, file1.getName().lastIndexOf('.')), URL + file.getPath().replace('\\', '/') + "/" + file1.getName(), URL + file.getPath().replace('\\', '/') + "/" + coverName));
+        return str;
     }
 }
+
